c8d4385a77b7b061aec3dbbc8c913804a1258c3c
Gyamfidouglas <gyamfidouglas9999@gmail.com>
2023-02-17 00:00:22 -0800

    Hell puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.
+/**
+ * main . A program that print a line with puts function
+ * Return: 0
+ */
+int main(void){
+	puts("\"progarmming is like building a multilingual puzzle");
+return: 0;
